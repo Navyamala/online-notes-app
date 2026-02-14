2cca6d59f263c00cd58ccfb2935a6c42095e2e12
Navyamala <ramshyamutivelly@gmail.com>
2026-02-14 14:29:03 +0530

    Prepare backend for deployment

--- notes-app/backend/server.js
+++ notes-app/backend/server.js
@@ -19,4 +19,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
